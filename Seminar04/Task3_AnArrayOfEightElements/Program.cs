1bb569621fcba41afec1f552780a308ba48586a2
LukashevichEvgeniy <humansyndr8me@gmail.com>
2022-08-07 16:58:34 +0800

    The 4th homework added

--- Seminar04/Task3_AnArrayOfEightElements/Program.cs
+++ Seminar04/Task3_AnArrayOfEightElements/Program.cs
@@ -2,7 +2,7 @@
 
 void ShowArray(int[] array)
 {
-    Console.ForegroundColor = ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.Blue;
     
     for (int index = 0; index < array.Length; index++)
         Console.Write($"{array[index]} ");
@@ -24,13 +24,19 @@ int[] GenerateArray(int size, int minValue, int maxValue)
 int size = 8, minValue, maxValue;
 int[] userArray = new int[size];
 
-Console.Write("Would you like to fill in the array yourself? (Yes / No) -");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Would you like to fill in the array yourself? (Yes / No): ");
+Console.ForegroundColor = ConsoleColor.White;
 string usersDecision = Console.ReadLine();
+Console.ResetColor();
+
 
 for (int count = 0; usersDecision != "Yes" && usersDecision != "No" && count < 3; count++)
     {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Just tell me, do you want to fill in this array yourself?");
         Console.Write($"You have {3-count} lives left. Yes or No: ");
+        Console.ResetColor();
         usersDecision = Console.ReadLine();
     }
 
@@ -38,23 +44,41 @@ if (usersDecision == "Yes")
 {
     for (int i = 0; i < size; i++)
     {
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.Write($"Input the {i+1} integer: ");
+        Console.ResetColor();
         userArray[i] = Convert.ToInt32(Console.ReadLine());
     }
+    
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Catch your array:");
     ShowArray(userArray);
+
 }
 
 if (usersDecision == "No")
 {
+    Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine("Okey, I'll do it for you :)");
     Console.Write("Just tell me the minimum value: ");
+    Console.ResetColor();
     minValue = Convert.ToInt32(Console.ReadLine());
+    
+    Console.ForegroundColor = ConsoleColor.Cyan;
     Console.Write("And the maximum value: ");
+    Console.ResetColor();
     maxValue = Convert.ToInt32(Console.ReadLine());
     
     userArray = GenerateArray(size, minValue, maxValue);
+    
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Catch your array:");
     ShowArray(userArray);
 
 }
+Console.ForegroundColor = ConsoleColor.DarkGray;
 Console.WriteLine("That's all. Try again, if you want");
+Console.ResetColor();
 
--- Seminar04/forReview/Program.cs
+++ Seminar04/forReview/Program.cs
@@ -65,12 +65,12 @@ Console.WriteLine($"The sum of the digits of {userNum} is {ssum}");
 
 
 // TASK 3. An array of eight elements
-// Конечно, задача в разы короче, но я хотел попробовать немного повзаимодействовать с пользователем ))
-
+// Конечно, задача в разы короче, но я хотел попробовать немного повзаимодействовать с пользователем :))
+// И попробавать немного цвета применить, чтобы всё не сливалось в общую массу текста в терминале
 
 void ShowArray(int[] array)
 {
-    Console.ForegroundColor = ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.Blue;
     
     for (int index = 0; index < array.Length; index++)
         Console.Write($"{array[index]} ");
@@ -92,13 +92,19 @@ int[] GenerateArray(int size, int minValue, int maxValue)
 int size = 8, minValue, maxValue;
 int[] userArray = new int[size];
 
-Console.Write("Would you like to fill in the array yourself? (Yes / No) -");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Would you like to fill in the array yourself? (Yes / No): ");
+Console.ForegroundColor = ConsoleColor.White;
 string usersDecision = Console.ReadLine();
+Console.ResetColor();
+
 
 for (int count = 0; usersDecision != "Yes" && usersDecision != "No" && count < 3; count++)
     {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Just tell me, do you want to fill in this array yourself?");
         Console.Write($"You have {3-count} lives left. Yes or No: ");
+        Console.ResetColor();
         usersDecision = Console.ReadLine();
     }
 
@@ -106,23 +112,41 @@ if (usersDecision == "Yes")
 {
     for (int i = 0; i < size; i++)
     {
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.Write($"Input the {i+1} integer: ");
+        Console.ResetColor();
         userArray[i] = Convert.ToInt32(Console.ReadLine());
     }
+    
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Catch your array:");
     ShowArray(userArray);
+
 }
 
 if (usersDecision == "No")
 {
+    Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine("Okey, I'll do it for you :)");
     Console.Write("Just tell me the minimum value: ");
+    Console.ResetColor();
     minValue = Convert.ToInt32(Console.ReadLine());
+    
+    Console.ForegroundColor = ConsoleColor.Cyan;
     Console.Write("And the maximum value: ");
+    Console.ResetColor();
     maxValue = Convert.ToInt32(Console.ReadLine());
     
     userArray = GenerateArray(size, minValue, maxValue);
+    
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Catch your array:");
     ShowArray(userArray);
 
 }
+Console.ForegroundColor = ConsoleColor.DarkGray;
 Console.WriteLine("That's all. Try again, if you want");
+Console.ResetColor();
 
